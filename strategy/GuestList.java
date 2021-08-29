dabd40d3197b1e3dac06e56d20494597cce7f7ca
Dreyson <dreyson@email.sc.edu>
2021-08-29 19:34:55 -0400

    started all classes and begun GuestList class variables

---
+++ strategy/GuestList.java
@@ -0,0 +1,16 @@
+package strategy;
+/**
+* Can add and remove people off list and checks to see if on list or not
+* @author Dreyson Clark
+*/
+
+import java.util.ArrayList;
+
+public class GuestList {
+ private String title;
+ private ArrayList<String> people;
+public GuestList(String title){
+
+}
+
+}

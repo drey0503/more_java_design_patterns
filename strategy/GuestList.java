84e60f9d005f3007b1f457a6c6b28ff2f263057c
Dreyson <dreyson@email.sc.edu>
2021-08-29 21:29:35 -0400

    finished GuestList file and SearchBehavior file

--- strategy/GuestList.java
+++ strategy/GuestList.java
@@ -1,4 +1,5 @@
 package strategy;
+
 /**
 * Can add and remove people off list and checks to see if on list or not
 * @author Dreyson Clark
@@ -7,9 +8,36 @@ package strategy;
 import java.util.ArrayList;
 
 public class GuestList {
- private String title;
- private ArrayList<String> people;
-public GuestList(String title){
+    private String title;
+    private ArrayList<String> people;
+    private SearchBehavior searchBehavior;
+
+    public GuestList(String title) {
+this.title=title;
+    }
+
+    public boolean add(String person){
+
+        if (searchBehavior.contains(people,person))
+
+        return false;
+
+        else{ people.add(person);
+
+            return true;
+}
+
+
+}
+public boolean remove(String person){
+
+    if (searchBehavior.contains(people,person)){
+    people.remove(person);
+    return true;}
+
+    return false;
+
+
 
 }
 
